0b0093f0de8a60e89acc575f53339394566ead77
cuberisu <cuberisu@gmail.com>
2021-10-25 00:03:52 +0900

    1-3

---
+++ Challenge_1-3.py
@@ -0,0 +1,22 @@
+# (1)
+import turtle
+t = turtle.Pen()
+t.shape("turtle")
+t.fd(100)
+t.left(120)
+t.fd(100)
+t.left(120)
+t.fd(100)
+
+# (2)
+import turtle
+t = turtle.Pen()
+t.shape("turtle")
+t.fd(100)
+t.left(90)
+t.fd(100)
+t.left(90)
+t.fd(100)
+t.left(90)
+t.fd(100)
+t._screen.exitonclick()     # 클릭해야 창이 닫히게 하는 코드
